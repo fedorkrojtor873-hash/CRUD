46b16381b4f2c08e59784592a259d70e115d032d
fedorkrojtor873-hash <fedorkrojtor873@gmail.com>
2026-02-02 18:52:42 +0200

    Finished delete all users

--- index.php
+++ index.php
@@ -29,16 +29,59 @@ include 'foo.php';
                 </thead>
                 <tbody>
 
-                <?php foreach ($result as $results) {;?>
-                <tr>
-                    <td><?php echo $results['id'] ?></td>
-                    <td><?php echo $results['name'] ?></td>
-                    <td><?php echo $results['email'] ?></td>
-                    <td>
-                        <a href="" class="btn btn-success"><i class="fa fa-edit"></i></a>
-                        <a href="" class="btn btn-danger"><i class="fa fa-trash-alt"></i></a>
-                    </td>
-                </tr>
+                <?php foreach ($result as $results) {
+                    ?>
+                    <tr>
+                        <td><?php echo $results['id'] ?></td>
+                        <td><?php echo $results['name'] ?></td>
+                        <td><?php echo $results['email'] ?></td>
+                        <td>
+                            <a href="?id=<?php echo $results['id']; ?>" class="btn btn-success" data-bs-toggle="modal"
+                               data-bs-target="#edit<?php echo $results['id'] ?>"><i class="fa fa-edit"></i></a>
+                            <a href="" class="btn btn-danger"><i class="fa fa-trash-alt"></i></a>
+                        </td>
+                    </tr>
+                    <!-- Modal Edit -->
+                    <div class="modal fade" id="edit<?php echo $results['id']; ?>" tabindex="-1"
+                         aria-labelledby="exampleModalLabel"
+                         aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Изменить запись</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="" method="post">
+                                        <input type="hidden" name="id" value="<?php echo $results['id']; ?>">
+                                        <small>Имя</small>
+                                        <div class="form-group">
+                                            <label>
+                                                <input type="text" class="form-control" name="name"
+                                                       value=' <?php echo $results['name'] ?>'>
+                                            </label>
+                                        </div>
+                                        <small>Email</small>
+                                        <div class="form-group">
+                                            <label>
+                                                <input type="text" class="form-control"
+                                                       name="email" value=' <?php echo $results['email'] ?> '>
+                                            </label>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                Закрыть
+                                            </button>
+                                            <button type="submit" class="btn btn-primary" name="edit">Изменить</button>
+
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Modal Edit -->
                 <?php } ?>
                 </tbody>
             </table>
